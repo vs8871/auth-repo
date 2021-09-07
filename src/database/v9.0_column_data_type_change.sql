263e8b1ed563cfa7d5006f4699c1218e7687d104
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-08 00:16:34 +0530

    Create v9.0_column_data_type_change.sql

---
+++ src/database/v9.0_column_data_type_change.sql
@@ -0,0 +1,6 @@
+ALTER TABLE order_info ALTER COLUMN quantity TYPE boolean,
+                       ALTER COLUMN customer_id TYPE integer;
+
+
+
+ALTER TABLE shipment_info ALTER COLUMN yard_name TYPE integer;
