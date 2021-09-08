735517de484ae81bc3ffc3b5485e395e71d114ab
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-08 15:53:33 +0530

    Update v10.0_column_rename.sql

--- src/database/v10.0_column_rename.sql
+++ src/database/v10.0_column_rename.sql
@@ -2,4 +2,4 @@ ALTER TABLE order_info RENAME COLUMN quantity TO new_quantity;
 ALTER TABLE order_info RENAME COLUMN customer_id TO new_customer_id;
 
 
-ALTER TABLE shipment_info RENAME COLUMN yard_name TO new_yard_name;
+ALTER TABLE shipment_info RENAME COLUMN yard_name TO new_yard_name; 
