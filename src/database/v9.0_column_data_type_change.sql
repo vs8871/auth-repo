227b1b4814529ce35e5b29b4b0946bc2171e8b62
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 00:04:54 +0530

    Update v9.0_column_data_type_change.sql

--- src/database/v9.0_column_data_type_change.sql
+++ src/database/v9.0_column_data_type_change.sql
@@ -1,5 +1,5 @@
 ALTER TABLE order_info ALTER COLUMN quantity TYPE boolean,
-                        ALTER COLUMN customer_id TYPE integer;
+                        ALTER COLUMN delivery_date TYPE varchar ( 30 ) ;
 
 
 
