de94a7213004e9e79827e22823f7f83eafc8cc0b
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-08 23:40:31 +0530

    Update v6.0_alter_table_script.sql

--- src/database/v6.0_alter_table_script.sql
+++ src/database/v6.0_alter_table_script.sql
@@ -4,4 +4,4 @@ ALTER TABLE order_info ADD COLUMN basic_template varchar ( 30 ) ,
 ALTER TABLE product_info ADD COLUMN product_serial_number varchar ( 30 ) ,
                            ADD COLUMN product_serial_id int ;
 
-ALTER TABLE   shipment_info ADD COLUMN dispatch_info varchar ( 30 ) ;
+ALTER TABLE   shipment_info ADD COLUMN dispatch_info varchar ( 30 ) ; 
