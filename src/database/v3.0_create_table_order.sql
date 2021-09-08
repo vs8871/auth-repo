d8673320f4d646471faa7dcfbd7b49de78d69e2e
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-08 22:49:02 +0530

    Update v3.0_create_table_order.sql

--- src/database/v3.0_create_table_order.sql
+++ src/database/v3.0_create_table_order.sql
@@ -5,3 +5,14 @@ CREATE TABLE product_info
   items integer,
   comment TEXT
 );
+
+
+CREATE TABLE accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
+
