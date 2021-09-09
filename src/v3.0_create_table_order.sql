bdf26ccc9e3381027b71ae46276773664e2b73aa
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 16:17:14 +0530

    Delete v3.0_create_table_order.sql

--- src/v3.0_create_table_order.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE order_info ADD COLUMN order_by varchar ( 30 ) ,
-                         ADD COLUMN pin_code int ;
-
-
-ALTER TABLE accounts ADD COLUMN dispatch_location varchar ( 30 ) ;
