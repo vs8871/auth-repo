6bce0d2e66c54eac0db660e5689dcf3d31cdb98b
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 15:56:54 +0530

    Delete v8.0_drop_column.sql

--- src/database/v8.0_drop_column.sql
+++
@@ -1,7 +0,0 @@
-ALTER   TABLE order_info DROP COLUMN basic_template,
-                         DROP   COLUMN order_e_mail_id  ;
-
-ALTER   TABLE product_info DROP COLUMN product_serial_number,
-                          DROP COLUMN product_serial_id;
-
-  ALTER TABLE shipment_info     DROP COLUMN   dispatch_info;
