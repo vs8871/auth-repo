660e97ab297ee14f4f37cab97538998443c6eb22
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 16:48:27 +0530

    Delete v4_0_drop_column_table.sql

--- src/v4_0_drop_column_table.sql
+++
@@ -1,7 +0,0 @@
-ALTER   TABLE order_info DROP COLUMN order_by,
-                         DROP   COLUMN pin_code  ; 
-
-ALTER   TABLE product_info DROP COLUMN receving_date,
-                          DROP COLUMN items;
-
-  ALTER TABLE accounts DROP COLUMN  last_login;
