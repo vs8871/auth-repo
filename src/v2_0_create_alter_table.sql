70963253102d4f8ebbc7ef6025480527988a481e
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 17:34:36 +0530

    Create v2_0_create_alter_table.sql

---
+++ src/v2_0_create_alter_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE accounts (
+	id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
+
+
+
+ALTER TABLE order_info ADD COLUMN address varchar(30);
