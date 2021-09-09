13369773867c4bde9acfcfb8f51c5b3f03737ea8
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 17:09:26 +0530

    Delete v5_0_column_data_type_change.sql

--- v5_0_column_data_type_change.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE order_info ALTER COLUMN quantity TYPE  varchar ( 40 ) ,
-                        ALTER COLUMN feedback TYPE integer ;
-
-
-ALTER TABLE accounts ALTER COLUMN created_on TYPE integer ;
