c6c8c98e038f2ce434b90798dc0bdee61d700b35
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 15:56:42 +0530

    Delete v6.0_alter_table_script.sql

--- src/database/v6.0_alter_table_script.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE order_info ADD COLUMN order_by varchar ( 30 ) ,
-                         ADD COLUMN pin_code int ;
-
-
-ALTER TABLE   shipment_info ADD COLUMN dispatch_location varchar ( 30 ) ;
