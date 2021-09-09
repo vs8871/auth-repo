56ef2a9bf1c92d017c74935889b68b2e44a89991
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 15:57:04 +0530

    Delete v9.0_column_data_type_change.sql

--- src/database/v9.0_column_data_type_change.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE order_info ALTER COLUMN quantity TYPE  varchar ( 40 ) ,
-                        ALTER COLUMN feedback TYPE integer ;
-
-
-
-ALTER TABLE shipment_info ALTER COLUMN yard_name TYPE integer ; 
