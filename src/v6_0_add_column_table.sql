eb59f2b19d42acdf6a9cb3f00e4624be8dc637b6
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-12 22:51:33 +0530

    Create v6_0_add_column_table.sql

---
+++ src/v6_0_add_column_table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE order_info ADD COLUMN order_by varchar ( 30 ) ,
+                         ADD COLUMN pin_code integer ;
+
+
+ALTER TABLE accounts ADD COLUMN dispatch_location varchar ( 30 ) 
