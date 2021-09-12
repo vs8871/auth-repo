6dc342376f8f63a9a1ce3425217fa0c5e71b71de
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-12 22:49:34 +0530

    Create v5_0_rename_column.sql

---
+++ src/v5_0_rename_column.sql
@@ -0,0 +1,5 @@
+  ALTER TABLE order_info  RENAME COLUMN  quantity   TO  new_quantity;
+  ALTER TABLE order_info   RENAME COLUMN   product_id  TO new_product_id;
+        
+      
+  ALTER TABLE accounts  RENAME COLUMN   created_on   TO new_created_on;
