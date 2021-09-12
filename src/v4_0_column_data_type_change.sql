6c7e10a4a8afec2314687326a2005cb0d70299bd
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-12 22:47:32 +0530

    Create v4_0_column_data_type_change.sql

---
+++ src/v4_0_column_data_type_change.sql
@@ -0,0 +1,5 @@
+ALTER TABLE order_info ALTER COLUMN quantity TYPE  varchar ( 40 ) ,
+                        ALTER COLUMN feedback TYPE integer ;
+
+
+ALTER TABLE accounts ALTER COLUMN created_on TYPE integer ;
